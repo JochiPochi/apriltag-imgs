e2de0cba26accef5d33c388e5ac763f0202d8f21
JochiPochi <aleman@mit.edu>
2022-01-14 18:27:34 -0500

    margins still exist

--- tag36h11/resize/resize.py
+++ tag36h11/resize/resize.py
@@ -7,6 +7,10 @@ num_x_plot = 4
 num_y_plot = 6
 plot_size_x = 8.0
 plot_size_y = 8.0
+tag_size_x = 7.0
+tag_size_y = 7.0
+plot_dpi = 300
+plot_margin = 0
 fig, axs = plt.subplots(num_y_plot, num_x_plot)
 fig.set_size_inches((num_x_plot*plot_size_x,num_y_plot*plot_size_y))
 for iy in range(num_y_plot):
@@ -27,16 +31,16 @@ for iy in range(num_y_plot):
         else:
             tag_str += "0"+str(tag_id)
         img = np.array(plt.imread("../tag36_11_{}.png".format(tag_str)))
-        mg = 20
-        img_resize = np.zeros((16*81+2*mg,16*81+2*mg,4))
+        img_resize = np.zeros(( int(plot_size_x*plot_dpi) + 2*plot_margin, int(plot_size_y*plot_dpi) + 2*plot_margin, 4))
         #np.set_printoptions(threshold=np.inf)
         #print(img_resize)
-        for i in range(9):
-            for j in range(9):
-                img_resize[mg+i*16*9:mg+(i+1)*16*9,mg+j*16*9:mg+(j+1)*16*9,:] = img[i,j,:]
+        for i in range(10):
+            for j in range(10):
+                img_resize[plot_margin+i*16*9:plot_margin+(i+1)*16*9,plot_margin+j*16*9:plot_margin+(j+1)*16*9,:] = img[i,j,:]
         
         ax.imshow(img_resize)
         
+        plt.imsave("test.png",img_resize, dpi=plot_dpi)
         #ax.set_title("tag_id: {}".format(tag_str), y=1.02, fontsize='30')
 
 plt.subplots_adjust(hspace=10.0)
