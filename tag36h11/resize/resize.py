2914e761ba502abdf6d088865dda65d787a2ad73
JochiPochi <aleman@mit.edu>
2022-01-14 19:15:19 -0500

    looking good

--- tag36h11/resize/resize.py
+++ tag36h11/resize/resize.py
@@ -7,10 +7,8 @@ num_x_plot = 4
 num_y_plot = 6
 plot_size_x = 8.0
 plot_size_y = 8.0
-tag_size_x = 7.0
-tag_size_y = 7.0
+tag_size = 7.0
 plot_dpi = 300
-plot_margin = 0
 fig, axs = plt.subplots(num_y_plot, num_x_plot)
 fig.set_size_inches((num_x_plot*plot_size_x,num_y_plot*plot_size_y))
 for iy in range(num_y_plot):
@@ -31,19 +29,21 @@ for iy in range(num_y_plot):
         else:
             tag_str += "0"+str(tag_id)
         img = np.array(plt.imread("../tag36_11_{}.png".format(tag_str)))
-        img_resize = np.zeros(( int(plot_size_x*plot_dpi) + 2*plot_margin, int(plot_size_y*plot_dpi) + 2*plot_margin, 4))
+        img_resize = np.zeros(( int(plot_size_x*plot_dpi), int(plot_size_y*plot_dpi), 4))
         #np.set_printoptions(threshold=np.inf)
         #print(img_resize)
-        for i in range(10):
-            for j in range(10):
-                img_resize[plot_margin+i*16*9:plot_margin+(i+1)*16*9,plot_margin+j*16*9:plot_margin+(j+1)*16*9,:] = img[i,j,:]
+        for i in range(1,9):
+            for j in range(1,9):
+                margin = int((plot_size_x-tag_size)*plot_dpi/2)
+                x_range = int(tag_size*plot_dpi/8)
+                y_range = int(tag_size*plot_dpi/8)
+                img_resize[margin+(i-1)*x_range: margin+(i)*x_range, margin+(j-1)*y_range: margin+(j)*y_range,:] = img[i,j,:]
         
         ax.imshow(img_resize)
-        
         plt.imsave("test.png",img_resize, dpi=plot_dpi)
-        #ax.set_title("tag_id: {}".format(tag_str), y=1.02, fontsize='30')
+        ax.set_title("tag_id: {}".format(tag_str), y=1.02, fontsize='30')
 
-plt.subplots_adjust(hspace=10.0)
+#plt.subplots_adjust(hspace=20.0)
 plt.tight_layout()
 plt.savefig("./apriltag_sample.pdf", format="pdf")
 plt.plot()
